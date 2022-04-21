9e942c028f21fb57a4a2119b13022b114c097124
tbriggs1 <tombriggs1993@outlook.com>
2022-04-21 16:27:33 +0100

    added user signup

--- src/components/registration/Register.js
+++ src/components/registration/Register.js
@@ -1,11 +1,16 @@
 import React from "react";
+import axios from "axios";
 
 const Register = ({un, pw, fn, ln, email}) => {
 
     const pdefaul = (e) => {
         e.preventDefault();
         
-        
+        const body = { "username": un, "firstname": fn, "lastname": ln, "email": email, "password": pw }
+        const headers = {
+            'Content-Type': 'application/json'
+        }
+        axios.post('http://127.0.0.1:5000/user', body, {headers})
     }
 
     return(
